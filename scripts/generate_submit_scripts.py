327f86e9cbc37091da3be859767a0769f5c45052
Songyuan Cui <cui20@illinois.edu>
2023-04-26 17:05:54 -0500

    feat: add weak scaling generating functions to generate_submit_scripts.py

--- scripts/generate_submit_scripts.py
+++ scripts/generate_submit_scripts.py
@@ -22,6 +22,35 @@ def generate_mpi_strong_scaling(num_threads, dims):
                 dim=dim,
             )
 
+def generate_omp_weak_scaling(num_threads, dims):
+    assert len(num_threads) == len(dims), (
+        "For weak scaling, num_threads and dims must have the same length"
+    )
+    for np, dim in zip(num_threads, dims):
+        write_submit_file(
+            dir="single_node_weak/",
+            key_string="omp_weak",
+            run_flag="o",
+            num_nodes=1,
+            np=np,
+            dim=dim,
+        )
+        
+def generate_mpi_weak_scaling(num_threads, dims):
+    assert len(num_threads) == len(dims), (
+        "For weak scaling, num_threads and dims must have the same length"
+    )
+    for np, dim in zip(num_threads, dims):
+        write_submit_file(
+            dir="across_node_weak/",
+            key_string="mpi_weak",
+            run_flag="m",
+            num_nodes=np,
+            np=np,
+            dim=dim,
+        )
+
+
 def write_submit_file(
     key_string: str,
     run_flag: str,
@@ -45,7 +74,7 @@ def write_submit_file(
             f"#SBATCH -p secondary\n",
             "\n",
             f"cd $SLURM_SUBMIT_DIR/build\n",    # Assume called from source tree
-            f"./Main -{run_flag} -p {np} -d {dim} > $SLURM_SUBMIT_DIR/result/{key_string}-dim-{dim}-np-{np}.txt"
+            f"./Main -{run_flag} -p {np} -d {dim} > $SLURM_SUBMIT_DIR/result/{key_string}-dim-{dim}-np-{np}.txt\n"
         ]
     )
     f.close()
@@ -56,6 +85,14 @@ if __name__ == "__main__":
         dims=[4096],
     )
     generate_mpi_strong_scaling(
-        num_threads=[1, 2, 4, 8],
+        num_threads=[1, 2, 4, 8, 16],
         dims=[4096],
+    )
+    generate_omp_weak_scaling(
+        num_threads=[1, 4, 16],
+        dims=[1024, 2048, 4096],
+    )
+    generate_mpi_weak_scaling(
+        num_threads=[1, 4, 16],
+        dims=[1024, 2048, 4096],
     )
